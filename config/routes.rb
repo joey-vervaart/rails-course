a30180ac66d95cd3c51991d55fda35532fd093d7
Joey Vervaart <Joey-vervaart@live.nl>
2012-05-22 13:27:30 +0200

    Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Blog::Application.routes.draw do
+  
+  devise_for :users
+
+  devise_for :models
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
