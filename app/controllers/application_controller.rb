69cc3aefe8bbe5e0598a08ec17eab6a8b20dab45
Joey Vervaart <Joey-vervaart@live.nl>
2012-05-29 15:33:12 +0200

    blog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_local
+  def set_local
+ I18n.locale = extract_locale_from_accept_language_header
+
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+
+
+  end
 end
+
+
